792b76ea11b613e9b6067ee019975a9d2f771a1e
“[ <153067235+MerihunH@users.noreply.github.com>
2024-01-24 17:55:56 -0800

    Header & Footer Container is added.

--- netflix_clone/src/component/Footer/Footer.js
+++ netflix_clone/src/component/Footer/Footer.js
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import "../Footer/footer.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="foooter_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li> Privacy</li>
+              <li>Contact us</li>
+              <div className=""></div>
+            
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Legal Notices</li>
+              <li>Do Not Sell or Share My Personal Information</li>
+            </ul>
+          </div>
 
-export default Footer
+          <div>
+            <ul>
+              <li>Git Card</li>
+              <li>Netflix shop</li>
+              <li>Cookies Preferences</li>
+              <li>Ad Choices</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_code">
+          <p>service code</p>
+        </div>
+        <div className="copy-write">&copy; 1997-2024 Netflix. inc.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- netflix_clone/src/component/Header/Header.js
+++ netflix_clone/src/component/Header/Header.js
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import "./Header.css";
+import Netflix from "../../asset/image/Netflex.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="header_outer_container">
+        <div className="header_container">
+          <div className="header_left">
+            <ul>
+              <li>
+                <img src={Netflix} alt="Netflix Log" width={100}></img>
+              </li>
+              <li>Home</li>
+              <li>Tv Show</li>
+              <li>Movies</li>
+              <li>New and Popular</li>
+              <li>My List</li>
+              <li>Browse by Languages</li>
+            </ul>
+          </div>
+          <div className="header_left">
+            <ul>
+              <li>
+                <SearchIcon />
+              </li>
+              <li>
+                <NotificationsNoneIcon />
+              </li>
+              <li>
+                <AccountBoxIcon />
+              </li>
+              <li>
+                <ArrowDropDownIcon />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
